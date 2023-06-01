5e6bc39db08b8863e0f82b5928b6018247af373a
law909 <balint.lovey@gmail.com>
2023-06-01 02:55:13 +0200

    helyesírás

--- resources/views/emails/subscriptionconfirmationrequest.blade.php
+++ resources/views/emails/subscriptionconfirmationrequest.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h2>Üdvözlünk a Bulletin Monitoron!</h2>
-<p>Ezzel az email címeddel felíratkoztál a "{{ $eventgenerator }}" "{{ $parameter }}" eseményére.</p>
-<p>Kérjük <a href="{{ $confirmurl }}">erősítsd meg</a> a felíratkozásodat.</p>
+<p>Ezzel az email címeddel feliratkoztál a "{{ $eventgenerator }}" "{{ $parameter }}" eseményére.</p>
+<p>Kérjük <a href="{{ $confirmurl }}">erősítsd meg</a> a feliratkozásodat.</p>
 </body>
 </html>
