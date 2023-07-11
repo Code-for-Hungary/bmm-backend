3b24730e515fc2d113874a2c63c3dfd7b61ba659
law909 <balint.lovey@gmail.com>
2023-07-11 05:41:27 +0200

    Figyusz

--- resources/views/emails/subscriptionconfirmationrequest.blade.php
+++ resources/views/emails/subscriptionconfirmationrequest.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<h2>Üdvözlünk a Bulletin Monitoron!</h2>
+<h2>Üdvözlünk a Figyuszon!</h2>
 <p>Ezzel az email címeddel feliratkoztál a "{{ $eventgenerator }}" "{{ $parameter }}" eseményére.</p>
 <p>Kérjük <a href="{{ $confirmurl }}">erősítsd meg</a> a feliratkozásodat.</p>
 </body>
--- resources/views/emails/subscriptionnotification.blade.php
+++ resources/views/emails/subscriptionnotification.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<h2>Esemény történt a Bulletin Monitoron!</h2>
+<h2>Esemény történt!</h2>
 <p>Az esemény kiváltója: "{{ $eventgenerator }}"</p>
 <p>Ezt figyeltük: "{{ $parameter }}"</p>
 <p>Az esemény tartalma: {!! $eventcontent !!}</p>
