619851e77cc3b9b3bfe38346afa7acb4ce2676e7
law909 <balint.lovey@gmail.com>
2023-07-30 01:43:56 +0200

    email design

--- resources/views/emails/subscriptionnotification.blade.php
+++ resources/views/emails/subscriptionnotification.blade.php
@@ -5,27 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('shared.style')
 </head>
-<body>
+<body style="
+    color: #46009e;
+    background-color: #F9F6F6;
+    font-family: 'Faune', Arial, sans-serif;
+    font-size: 20px;
+    line-height: 1.3;
+">
 @include('shared.header')
-<h3>Szia!</h3>
+<h3 style="
+    font-size: 1.4rem;
+    font-weight: 700;
+">Szia!</h3>
 @switch($eventtype)
     @case(1)
-        <h3>Ez itt a Figyusz! adatbázis figyelő. Az általad megadott kulcszóra találat érkezett.</h3>
+        <h3 style="
+    font-size: 1.4rem;
+    font-weight: 700;
+">Ez itt a Figyusz! adatbázis figyelő. Az általad megadott kulcszóra találat érkezett.</h3>
         <div>
-            <span class="bmm-label">A találat helye: </span><span class="bmm-data">"{{ $eventgenerator }}"</span>
-            <span class="bmm-label">Kulcsszó: </span><span class="bmm-data">"{{ $parameter }}"</span>
+            <div><span class="bmm-label">A találat helye: </span><span class="bmm-data">"{{ $eventgenerator }}"</span></div>
+            <div><span class="bmm-label">Kulcsszó: </span><span class="bmm-data">"{{ $parameter }}"</span></div>
         </div>
         <div>
-            <span class="bmm-label">A találat tartalma: </span>{!! $eventcontent !!}
+            <div><span class="bmm-label">A találat tartalma: </span></div>{!! $eventcontent !!}
         </div>
         @break
     @case(2)
-        <h3>Ez itt a Figyusz! adatbázis figyelő. A figyelt adatbázisba új adat érkezett.</h3>
+        <h3 style="
+    font-size: 1.4rem;
+    font-weight: 700;
+">Ez itt a Figyusz! adatbázis figyelő. A figyelt adatbázisba új adat érkezett.</h3>
         <div>
             <span class="bmm-label">A találat helye: </span><span class="bmm-data">"{{ $eventgenerator }}"</span>
         </div>
         <div>
-            <span class="bmm-label">A találat tartalma: </span>{!! $eventcontent !!}
+            <div><span class="bmm-label">A találat tartalma: </span></div>{!! $eventcontent !!}
         </div>
         @break
 @endswitch
