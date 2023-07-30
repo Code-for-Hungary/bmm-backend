960b70f4e79cd958bf5eb4a1c6c0921c72506d47
law909 <balint.lovey@gmail.com>
2023-07-30 02:04:49 +0200

    email design

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,15 +1,69 @@
-<div class="bmm-footer-grid">
-    <div class="bmm-c4hu">
-        <a href="https://code4.hu/">
-            <img alt="C4HU" class="bmm-footer-logo" src="https://figyusz.k-monitor.hu/img/c4hu-logo.svg">
-        </a>
-    </div>
-    <div class="bmm-kmonitor">
-        <a href="https://k-monitor.hu/">
-            <img alt="K-Monitor" class="bmm-footer-logo" src="https://figyusz.k-monitor.hu/img/kmonitor-white.png">
-        </a>
-    </div>
-    <div class="bmm-adatkezelesi">
-        <a href="https://adatbazis.k-monitor.hu/egyeb/adatkezelesi-tajekoztato">Adatkezelési tájékoztató</a> &copy; 2023
+<div
+    class="bmm-footer"
+    style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.8rem;
+        margin-top: 5vh;
+        min-height: 14vh;
+        background-color: #FF3366;
+        color: #F9F6F6;
+        "
+>
+    <div
+        class="bmm-footer-grid"
+        style="
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 0px 0.5rem;
+            grid-template-areas:
+                'c4hu kmonitor'
+                'adatkezelesi adatkezelesi';
+            "
+    >
+        <div
+            class="bmm-c4hu"
+            style="
+                grid-area: c4hu;
+                align-self: center;
+                justify-self: right;
+            "
+        >
+            <a href="https://code4.hu/">
+                <img
+                    alt="C4HU"
+                    class="bmm-footer-logo"
+                    style="width: 30vw;"
+                    src="https://figyusz.k-monitor.hu/img/c4hu-logo.svg"
+                >
+            </a>
+        </div>
+        <div
+            class="bmm-kmonitor"
+            style="
+                grid-area: kmonitor;
+                align-self: center;
+            "
+        >
+            <a href="https://k-monitor.hu/">
+                <img
+                    alt="K-Monitor"
+                    class="bmm-footer-logo"
+                    style="width: 30vw;"
+                    src="https://figyusz.k-monitor.hu/img/kmonitor-white.png"
+                >
+            </a>
+        </div>
+        <div
+            class="bmm-adatkezelesi"
+            style="
+                grid-area: adatkezelesi;
+                align-self: center;
+                justify-self: center;
+            "
+        >
+            <a href="https://adatbazis.k-monitor.hu/egyeb/adatkezelesi-tajekoztato">Adatkezelési tájékoztató</a> &copy; 2023
+        </div>
     </div>
 </div>
