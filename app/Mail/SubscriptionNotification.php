8750840ea1b0b4824eef21bb03206be124705691
law909 <balint.lovey@gmail.com>
2023-09-18 23:24:03 +0200

    unsubscribe url

--- app/Mail/SubscriptionNotification.php
+++ app/Mail/SubscriptionNotification.php
@@ -51,8 +51,6 @@ class SubscriptionNotification extends Mailable
      */
     public function content(): Content
     {
-        Log::debug(config('bmm.unsubscribe_url'));
-        Log::debug(config('bmm.confirmation_frontend_url'));
         return new Content(
             view: 'emails.subscriptionnotification',
             with: [
