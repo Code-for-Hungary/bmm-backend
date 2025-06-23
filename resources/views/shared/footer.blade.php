40bfcd2419cce5e2e039d9a1a3a065faab494f39
boa <boa@protonmail.com>
2025-06-23 12:48:10 +0200

    remove copyright notice

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -7,7 +7,7 @@
             <a href="https://k-monitor.hu/">K-Monitor</a>
         </div>
         <div class="bmm-adatkezelesi">
-            <a href="https://adatbazis.k-monitor.hu/egyeb/adatkezelesi-tajekoztato">Adatkezelési tájékoztató</a> &copy; 2025
+            <a href="https://adatbazis.k-monitor.hu/egyeb/adatkezelesi-tajekoztato">Adatkezelési tájékoztató</a>
         </div>
     </div>
 </div>
